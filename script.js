b602b9abf3f6f0f45d2b265a7773b26a61b4b8ac
NicholeSteig <nichole.steig@gmail.com>
2020-03-13 23:55:24 -0700

    working on script

--- script.js
+++ script.js
@@ -1,60 +1,126 @@
-var inquirer = require("inquirer");
-var fs = require('fs');
-var Manager = require("./manager.html");
-var Employee = require("./e")
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const Questions = require("./lib/Questions")
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+function init() {
+    console.log("Welcome to the team builder")
+    staffQuestion();
+}
+
+function staffQuestion() {
+    inquirer.prompt(Questions.positionQuestion)
+        .then(function (ans) {
+            switch (ans.position) {
+                case "Engineer":
+                    buildEngineer();
+                    break;
+
+                case "Intern":
+                    buildIntern();
+                    break;
+
+                case "Manager":
+                    buildManager();
+                    break;
+            }
+        })
+}
+
+function buildEngineer() {
+    inquirer.prompt(Questions.engineerQuestions)
+        .then(function (data) {
+            let tempObj = new Engineer(data.engineer, data.engineerID, data.engineerName, data.engineerEmail, data.engineerGitHub);
+            objToHTML.push(tempObj);
+            buildStaff();
+        })
+}
+
+function buildIntern() {
+    inquirer.prompt(Questions.internQuestions)
+        .then(function (data) {
+            let tempObj = new Intern(data.intern, data.internID, data.internName, data.internEmail, data.internSchool);
+            objToHTML.push(tempObj);
+            buildStaff();
+        })
+}
+
+function buildManager() {
+    inquirer.prompt(Questions.managerQuestions)
+        .then(function (data) {
+            let tempObj = new Manager(data.manager, data.managerID, data.managerName, data.managerEmail, data.managerNumber);
+            objToHTML.push(tempObj);
+            buildStaff();
+        })
+}
+
+function buildHTML() {
+    let html = fs.readFileSync("./templates/index.html", "utf8");
+    let $index = cheerio.load(html);
+    $index("#addMember").html("");
+    writefileAsync("./templates/index.html", $index.html())
+        .then(function () {
+            addEmployeeHTML();
+        })
+}
+
 
 // Employee:
 // * name
 // * id
 // * title
-inquirer.prompt([
-    {
-        type: "input",
-        name: "name",
-        message: "What is your name?"
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is your ID number?"
-    },
-    {
-        type: "checkbox",
-        name: "title",
-        choices: [
-            "Manager",
-            "Engineer",
-            "Intern"
-        ]
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is your email address?"
-    }
+// inquirer.prompt([
+//     {
+//         type: "input",
+//         name: "name",
+//         message: "What is your name?"
+//     },
+//     {
+//         type: "input",
+//         name: "id",
+//         message: "What is your ID number?"
+//     },
+//     {
+//         type: "checkbox",
+//         name: "title",
+//         choices: [
+//             "Manager",
+//             "Engineer",
+//             "Intern"
+//         ]
+//     },
+//     {
+//         type: "input",
+//         name: "email",
+//         message: "What is your email address?"
+//     }
 
-]).then(function (input) {
-    role = input.role;
-    name = input.name;
-    id = input.id;
-    email = email.id;
+// ]).then(function (input) {
+//     role = input.role;
+//     name = input.name;
+//     id = input.id;
+//     email = email.id;
 
-    class Employee {
-        constructor(role, name, id, email) {
-            this.role = role;
-            this.name = name;
-            this.id = id;
-            this.email = email;
-        }
+//     class Employee {
+//         constructor(role, name, id, email) {
+//             this.role = role;
+//             this.name = name;
+//             this.id = id;
+//             this.email = email;
+//         }
 
-        getData() {
-            console.log(this.role);
-            console.log(this.name);
-            console.log(this.id);
-            console.log(this.email);
-        };
-    }
-    module.exports = Employee;
+//         getData() {
+//             console.log(this.role);
+//             console.log(this.name);
+//             console.log(this.id);
+//             console.log(this.email);
+//         };
+//     }
+//     module.exports = Employee;
 
     // if (this.title === "Manager") {
     //     enquirer.prompt([
@@ -110,7 +176,7 @@ inquirer.prompt([
     //         };
     //     });
     // };
-});
+// });
 // * getName()
 // * getId()
 // * getEmail()
